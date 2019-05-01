906c01d4c3b373b989b0f5dd0a79c416e8ee0a74
jungwan <jungwan82@naver.com>
2019-05-01 13:52:45 +0900

    Initial commit. 1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,61 @@
 # doecs
 Data Oriented Entity Component System framework.
+
+# How to use
+```		
+// Step 1. Define components, maybe in <EntityName>.h file
+struct PositionComponent
+{
+	float x, y, z;
+};
+
+struct RotationComponent
+{
+	float x, y, z, w;
+};
+
+// Step 2. Declare Archetype Pool for the new entity type in <EntityName>.h file (e.g. PlayerEntity.h)
+#define PlayerComponents PositionComponent, RotationComponent
+DeclareEntityArchetypePool(APlayerPool, PlayerComponents);
+
+// This lines could be located in another header file EnemyEntity.h for example.
+#define EnemyComponents PositionComponent
+DeclareEntityArchetypePool(AEnemyPool, EnemyComponents);
+
+// Step 3. Add new EntityArchetypePools to the global tuple for EntityPools in <MyEntitySystem>.h file (e.g. EntitySystem.h)
+extern std::tuple<APlayerPool, AEnemyPool> EntityPools;
+
+// Step 4. Also need to add the new pool to the actual tuple implementation in <MyEntitySystem>.cpp file
+std::tuple<APlayerPool, AEnemyPool> EntityPools;
+
+// Step 5. Implementation for Pools goes to each of header files. (e.g. PlayerEntity.cpp and EnemyEntity.cpp)
+ImplementEntityArchetypePool(APlayerPool);
+ImplementEntityArchetypePool(AEnemyPool);
+
+// Step 6. Define Systems in a new header file (e.g. MovementSystem.h)
+struct MovementSystem : public de::System<PositionComponent>
+{
+	void Execute(PositionComponent& pos)
+	{
+		// move it
+	}
+};
+
+// Step 7. Use defined entity and system like this
+void Test()
+{
+	// need to call once per new pool
+	InitializeEntityArchetypePool(APlayerPool);
+	InitializeEntityArchetypePool(AEnemyPool);
+
+	auto entityId = CreateEntity<PlayerComponents>();
+	auto entityId2 = CreateEntity<EnemyComponents>();
+
+	MovementSystem* pMoveSystem = new MovementSystem;
+	RunSystem(pMoveSystem);
+	delete pMoveSystem;
+
+	DestroyEntityArchetypePool(APlayerPool);
+	DestroyEntityArchetypePool(AEnemyPool);
+}
+```
---
+++ doecs.h
@@ -0,0 +1,237 @@
+#include <unordered_map>
+#include <array>
+#include <cstdlib>
+#include <mutex>
+
+// Create a cpp file and define IMPLEMENT_DOECS then include this header file.
+//#define IMPLEMENT_DOECS
+
+
+
+#define DeclareEntityArchetypePool(PoolName, ...) using PoolName = de::ArchetypePool<__VA_ARGS__>
+#define ImplementEntityArchetypePool(PoolName) PoolName::Chunk* PoolName::RootChunk = nullptr;\
+	std::unordered_map<de::EntityId, std::pair<PoolName::Chunk*, uint32_t> > PoolName::EntityToComponent
+#define InitializeEntityArchetypePool(PoolName) PoolName::Initialize()
+#define DestroyEntityArchetypePool(PoolName) PoolName::Destroy();
+
+// de stands for Do Ecs
+namespace de
+{
+	using EntityId = uint64_t;
+	constexpr int ChunkSize = 16 * 1024; // Usually CPU has 32 kb L1 cache and I decide to use half of it.
+	constexpr int CacheLineSize = 64;
+	const EntityId INVALID_ENTITY_ID = -1;
+	class FEntityIdGen {
+		std::mutex Mutex;
+		EntityId NextId = 1;
+
+	public:
+		FEntityIdGen() = default;
+		FEntityIdGen(EntityId startId)
+			: NextId(startId) {}
+
+		inline EntityId Gen() {
+			std::lock_guard l(Mutex);
+			return NextId++;
+		}
+	};
+	extern FEntityIdGen EntityIdGen;
+#ifdef IMPLEMENT_DOECS
+	FEntityIdGen EntityIdGen;
+#endif
+
+	//
+	// SizeOf
+	//
+	template < typename ... Types >
+	struct SizeOf;
+
+	template < typename TFirst >
+	struct SizeOf < TFirst >
+	{
+		static const auto Value = sizeof(TFirst);
+	};
+
+	template < typename TFirst, typename ... TRemaining >
+	struct SizeOf < TFirst, TRemaining ... >
+	{
+		static const auto Value = (sizeof(TFirst) + SizeOf<TRemaining...>::Value);
+	};
+
+	//
+	// has_type
+	//
+	template <typename T, typename Tuple>
+	struct has_type;
+
+	template <typename T, typename... Us>
+	struct has_type<T, std::tuple<Us...>> : std::disjunction<std::is_same<T, Us>...> {};
+
+	//
+	// intersection
+	//
+	template <typename S1, typename S2>
+	struct intersect
+	{
+		template <std::size_t... Indices>
+		static constexpr auto make_intersection(std::index_sequence<Indices...>) {
+
+			return std::tuple_cat(
+				std::conditional_t<
+				has_type<std::tuple_element_t<Indices, S1>, S2>::value,
+				std::tuple<std::tuple_element_t<Indices, S1>>,
+				std::tuple<>
+				>{}...);
+		}
+		using type = decltype(make_intersection(std::make_index_sequence<std::tuple_size<S1>::value>{}));
+	};
+
+	template<typename SubType, typename CheckingType, typename = void>
+	struct has_all_type;
+
+	template<typename SubType, typename CheckingType>
+	struct has_all_type<SubType, CheckingType, std::enable_if_t< std::is_same_v<typename intersect<SubType, CheckingType>::type, SubType>>>
+		: std::true_type
+	{
+	};
+
+	template<typename SubType, typename CheckingType>
+	struct has_all_type<SubType, CheckingType, std::enable_if_t< !std::is_same_v<typename intersect<SubType, CheckingType>::type, SubType>>>
+		: std::false_type
+	{
+	};
+
+	//
+	// ArchetypePool
+	//
+	template<typename ... ComponentTypes>
+	class ArchetypePool
+	{
+	public:
+		using Tuple = std::tuple<ComponentTypes...>;
+		static constexpr uint32_t EntitySize = SizeOf<ComponentTypes...>::Value;
+		static constexpr uint32_t ComponentCount = sizeof...(ComponentTypes);
+		static constexpr uint32_t ElementCountPerChunk = (ChunkSize - sizeof(void*) - sizeof(uint32_t)) / EntitySize;
+		struct Chunk
+		{
+		public:
+			std::tuple<std::array<ComponentTypes, ElementCountPerChunk>...> Components;
+			uint32_t Count = 1;
+			Chunk* Next = nullptr;
+
+			void* operator new(std::size_t size)
+			{
+#ifdef _MSC_VER
+				return _aligned_malloc(size, CacheLineSize);
+#else
+				// not tested.
+				return std::aligned_alloc(size, CacheLineSize);
+#endif
+			}
+
+			void operator delete(void* p)
+			{
+
+#ifdef _MSC_VER
+				_aligned_free(p);
+#else
+				std::free(p);
+#endif
+			}
+		};
+		static Chunk* RootChunk;
+		static std::unordered_map<EntityId, std::pair<Chunk*, uint32_t> > EntityToComponent;
+
+		static void Initialize()
+		{
+			RootChunk = new Chunk;
+		}
+
+		static void Destroy()
+		{
+			auto next = RootChunk->Next;
+			delete RootChunk;
+			while (next)
+			{
+				auto p = next;
+				delete p;
+				next = next->Next;
+			}
+		}
+		static EntityId CreateEntity()
+		{
+			static_assert(ElementCountPerChunk > 50, "Entity is too big");
+			auto entityId = EntityIdGen.Gen();
+			auto chunk = RootChunk;
+			while (chunk)
+			{
+				if (chunk->Count < ElementCountPerChunk)
+				{
+					auto componentIndex = chunk->Count++;
+					EntityToComponent[entityId] = { chunk, componentIndex };
+					return entityId;
+				}
+				if (!chunk->Next)
+				{
+					// create new chunk
+					chunk->Next = new Chunk;
+				}
+				chunk = chunk->Next;
+			}
+			return INVALID_ENTITY_ID;
+		}
+
+		template<typename SystemType, typename ... ComponentTypes>
+		static void RunSystem(SystemType* system, std::tuple<ComponentTypes...>& dummy)
+		{
+			auto chunk = RootChunk;
+			while (chunk)
+			{
+				for (uint32_t i = 0; i < chunk->Count; ++i)
+				{
+					system->Execute(std::get< std::array<ComponentTypes, ElementCountPerChunk>>(chunk->Components)[i]...);
+				}
+				chunk = chunk->Next;
+			}			
+		}
+	};
+
+
+	template<typename ... ComponentTypes>
+	struct System
+	{
+		using Tuple = std::tuple<ComponentTypes...>;
+	};
+
+	template<std::size_t I, typename SystemType, typename... Tp>
+	typename std::enable_if_t<I == sizeof...(Tp)> RunSystemImpl(std::tuple<Tp...>& pools, SystemType* system)
+	{ }
+
+
+	template<std::size_t I = 0, typename SystemType, typename... Tp>
+	typename std::enable_if_t < I < sizeof...(Tp)> RunSystemImpl(std::tuple<Tp...>& pools, SystemType* system)
+	{
+		auto& pool = std::get<I>(pools);
+		if constexpr (has_all_type<SystemType::Tuple, std::remove_reference<decltype(pool)>::type::Tuple>::value)
+		{
+			pool.RunSystem(system, SystemType::Tuple{});
+		}
+
+		RunSystemImpl<I + 1>(pools, system);
+	}
+
+	// struct MovementSystem : public System<PositionComponent>
+	template<typename SystemType>
+	void RunSystem(SystemType* system)
+	{
+		RunSystemImpl(EntityPools, system);
+	}
+
+
+	template<typename ... ComponentTypes>
+	EntityId CreateEntity()
+	{
+		return ArchetypePool<ComponentTypes...>::CreateEntity();
+	}
+}
+
